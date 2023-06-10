70f7e1669e2b853e5586c2be1f5b0f4b95bdda95
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2023-06-10 17:48:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//! |> Start Dark Mode
+const toggleSwitch = document.getElementById("toggleSwitch");
+circle = document.getElementById("circle");
+body = document.querySelector("body");
+
+toggleSwitch.onclick = function () {
+  toggleSwitch.classList.toggle("dark");
+  circle.classList.toggle("dark");
+  body.classList.toggle("dark");
+};
+//! |> Start Progress
+let section = document.querySelector(".skills");
+let spans = document.querySelectorAll(".progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop + 100) {
+    console.log("good");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+//! |> Toggle Scroll Top
+const scrollTop = document.getElementById("scrollTop");
+(window.onscroll = function () {
+  window.scrollY >= 600
+    ? (scrollTop.style.display = "block")
+    : (scrollTop.style.display = "none");
+}),
+  (scrollTop.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
